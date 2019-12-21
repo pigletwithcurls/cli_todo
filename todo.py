d88d87ab0855472b632bd9b07949142fe329bac2
Sunmin <pigletwithcurls@gmail.com>
2019-12-21 12:31:08 -0500

    Add user interactive bar
    We add a user interactive bar at the bottom of the terminal for users
    to be able to enter one of the choices that they desire:
      * addTodoItem
      * moveToInProgress
      * moveToCompleted

--- todo.py
+++ todo.py
@@ -3,103 +3,142 @@
 Vertical align demo with VSplit.
 """
 from prompt_toolkit.application import Application
-from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.document import Document
 from prompt_toolkit.formatted_text import HTML
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.layout.containers import (
     HSplit,
-    VerticalAlign,
     VSplit,
     Window,
     WindowAlign,
 )
-from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
-from prompt_toolkit.layout.dimension import D
+from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
-from prompt_toolkit.widgets import Frame
+from prompt_toolkit.widgets import Frame, TextArea
 
 TITLE = HTML(
     """<u>Todo Example</u> experiment.
- Press <b>'q'</b> to quit."""
-)
+ Press <b>'q'</b> to quit.
 
-LIPSUM = """
-TODO EXAMPLE."""
+ At the beginning of prompt:
+ Add 'add' to add a todo item to TODO list.
+ Add 'mp [item index]' to move a todo item from TODO list to In Progress.
+ Add 'mc [item index]' to move a todo item from In Progress to Completed.
+ """
+)
 
 todo_list = []
 inprogress_list = []
 completed_list = []
 
+input_field = TextArea(
+    height=1,
+    prompt=">>> ",
+    style="class:input-field",
+    multiline=False,
+    wrap_lines=False,
+)
+
+
+todo_field = TextArea(style="class:output-field")
+inprogress_field = TextArea(style="class:output-field")
+completed_field = TextArea(style="class:output-field")
+
+categories = [
+    Window(
+        FormattedTextControl(HTML("<u>TODO List</u>")),
+        height=4,
+        ignore_content_width=True,
+        style="bg:#70cb98 #000000 bold",
+        align=WindowAlign.CENTER,
+    ),
+    Window(
+        FormattedTextControl(HTML("<u>In Progress</u>")),
+        height=4,
+        ignore_content_width=True,
+        style="bg:#70cb98 #000000 bold",
+        align=WindowAlign.CENTER,
+    ),
+    Window(
+        FormattedTextControl(HTML("<u>TODO Completed</u>")),
+        height=4,
+        ignore_content_width=True,
+        style="bg:#70cb98 #000000 bold",
+        align=WindowAlign.CENTER,
+    ),
+]
+
+
+def accept(buff):
+    try:
+        if input_field.text[:3] == "add":
+            addTodo(input_field.text[4:])
+        elif input_field.text[:2] == "mp":
+            moveToInProgress(input_field.text[3])
+        elif input_field.text[:2] == "mc":
+            moveToCompleted(input_field.text[3])
+    except BaseException as e:
+        print("\nError", e)
+
+
+input_field.accept_handler = accept
+
 
 def addTodo(todo):
-    if len(todo_list) >= 5:
-        return
-    todo_item = Window(
-        BufferControl(Buffer(document=Document(todo))), height=2, style="bg:#000000"
-    )
-    todo_list.append(todo_item)
+    todo_list.append(todo)
+    refreshTodo()
 
 
-addTodo("Grocery shopping for family")
-addTodo("Cook for lunch next week")
+def refreshTodo():
+    todo_text = ""
+    for i in range(len(todo_list)):
+        todo_text += "[" + str(i) + "] " + todo_list[i] + "\n"
+    todo_field.buffer.document = Document(text=todo_text)
+
+
+def moveToInProgress(todo_index):
+    item = todo_list[int(todo_index)]
+    todo_list.pop(int(todo_index))
+    refreshTodo()
+    inprogress_list.append(item)
+    refreshInProgress()
+
+
+def refreshInProgress():
+    inprogress_text = ""
+    for i in range(len(inprogress_list)):
+        inprogress_text += "[" + str(i) + "] " + inprogress_list[i] + "\n"
+    inprogress_field.buffer.document = Document(text=inprogress_text)
+
+
+def moveToCompleted(inprogress_index):
+    item = inprogress_list[int(inprogress_index)]
+    inprogress_list.pop(int(inprogress_index))
+    refreshInProgress()
+    completed_list.append(item)
+    refreshCompleted()
+
+
+def refreshCompleted():
+    completed_text = ""
+    for i in range(len(completed_list)):
+        completed_text += "[" + str(i) + "] " + completed_list[i] + "\n"
+    completed_field.buffer.document = Document(text=completed_text)
+
 
 # 1. The layout
 body = HSplit(
     [
         Frame(
-            Window(FormattedTextControl(TITLE), height=2), style="bg:#000000 #ffffff"
+            Window(FormattedTextControl(TITLE), height=7), style="bg:#000000 #ffffff"
         ),
-        VSplit(
-            [
-                Window(
-                    FormattedTextControl(HTML(" <u>TODO List</u>")),
-                    height=4,
-                    ignore_content_width=True,
-                    style="bg:#000000 #ffffff bold",
-                    align=WindowAlign.CENTER,
-                ),
-                Window(
-                    FormattedTextControl(HTML(" <u>In Progress</u>")),
-                    height=4,
-                    ignore_content_width=True,
-                    style="bg:#000000 #ffffff bold",
-                    align=WindowAlign.CENTER,
-                ),
-                Window(
-                    FormattedTextControl(HTML(" <u>TODO Completed</u>")),
-                    height=4,
-                    ignore_content_width=True,
-                    style="bg:#000000 #ffffff bold",
-                    align=WindowAlign.CENTER,
-                ),
-            ],
-            height=1,
-            padding=4,
-            padding_style="bg:#000000",
-        ),
+        VSplit(categories, height=1, padding=4, padding_style="bg:#000000",),
         VSplit(
             [
                 # TODO List
                 Frame(
                     HSplit(
-                        [
-                            Window(
-                                BufferControl(Buffer(document=Document(LIPSUM))),
-                                height=6,
-                                style="bg:#000000",
-                            ),
-                            Window(
-                                BufferControl(Buffer(document=Document(LIPSUM))),
-                                height=6,
-                                style="bg:#000000",
-                            ),
-                            Window(
-                                BufferControl(Buffer(document=Document(LIPSUM))),
-                                height=6,
-                                style="bg:#000000",
-                            ),
-                        ],
+                        [todo_field],
                         padding=1,
                         padding_style="bg:#000000",
                         padding_char="-",
@@ -108,13 +147,7 @@ body = HSplit(
                 # In Progress
                 Frame(
                     HSplit(
-                        [
-                            Window(
-                                BufferControl(Buffer(document=Document(LIPSUM))),
-                                height=6,
-                                style="bg:#000000",
-                            )
-                        ],
+                        [inprogress_field],
                         padding=1,
                         padding_style="bg:#000000",
                         padding_char="-",
@@ -123,13 +156,7 @@ body = HSplit(
                 # TODO Completed
                 Frame(
                     HSplit(
-                        [
-                            Window(
-                                BufferControl(Buffer(document=Document(""))),
-                                height=6,
-                                style="bg:#000000",
-                            ),
-                        ],
+                        [completed_field],
                         padding=1,
                         padding_style="bg:#000000",
                         padding_char="-",
@@ -140,6 +167,8 @@ body = HSplit(
             padding_style="bg:#000000 #ffffff",
             padding_char=".",
         ),
+        Window(height=1, char="-", style="class:line"),
+        input_field,
     ]
 )
 
@@ -153,13 +182,13 @@ def _(event):
     event.app.exit()
 
 
-@kb.add("tab")
-def _(event):
-    event.app.layout.focus_next()
-
-
 # 3. The `Application`
-application = Application(layout=Layout(body), key_bindings=kb, full_screen=True)
+application = Application(
+    layout=Layout(body, focused_element=input_field),
+    key_bindings=kb,
+    mouse_support=True,
+    full_screen=True,
+)
 
 
 def run():
